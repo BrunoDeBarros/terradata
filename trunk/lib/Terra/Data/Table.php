dd1dc7a7e4326c0b97c1c611fea68a5b857edf10
bruno.terraduo@gmail.com <bruno.terraduo@gmail.com@c9d6c2dd-1101-386e-3e53-6a280b27bd08>
2010-12-09 13:43:37 +0000

    Added table discovery via discoverTable() and exportCode() to export a Terra_Data_Table into valid PHP code.

--- trunk/lib/Terra/Data/Table.php
+++ trunk/lib/Terra/Data/Table.php
@@ -14,7 +14,6 @@
  * file that was distributed with this source code.
  */
 class Terra_Data_Table implements ArrayAccess {
-    
     const MANAGE = 'Manage';
     const VIEW = 'View';
     const EDIT = 'Edit';
@@ -38,18 +37,21 @@ class Terra_Data_Table implements ArrayAccess {
         'Fields' => array()
     );
 
-    function __construct($Table) {
+    function __construct($Table, $AutoDiscover = false) {
         $this->Container['Name'] = $Table;
+        if ($AutoDiscover) {
+            self::discoverTable($Table, null, &$this);
+        }
     }
-    
+
     function setSingular($Singular) {
         $this->Container['Singular'] = $Singular;
     }
-    
+
     function setPlural($Plural) {
         $this->Container['Plural'] = $Plural;
     }
-    
+
     function setHtmlTemplate($HtmlTemplate) {
         $this->Container['HtmlTemplate'] = $HtmlTemplate;
     }
@@ -57,23 +59,23 @@ class Terra_Data_Table implements ArrayAccess {
     function setManageUrl($Url) {
         return $this->setUrl($Url, 'Manage', array('PAGE', 'ROWS_PER_PAGE'));
     }
-    
+
     function setViewUrl($Url) {
         return $this->setUrl($Url, 'View', array('ID'));
     }
-    
+
     function setDeleteUrl($Url) {
         return $this->setUrl($Url, 'Delete', array('ID'));
     }
-    
+
     function setRestoreUrl($Url) {
         return $this->setUrl($Url, 'Restore', array('ID'));
     }
-    
+
     function setEditUrl($Url) {
         return $this->setUrl($Url, 'Edit', array('ID'));
     }
-    
+
     function setCreateUrl($Url) {
         return $this->setUrl($Url, 'Create');
     }
@@ -113,7 +115,7 @@ class Terra_Data_Table implements ArrayAccess {
             'ValidationRules' => array()
         );
     }
-    
+
     function allowField($FieldIdentifier, $Action) {
         $this->Container['Fields'][$FieldIdentifier][$Action] = true;
     }
@@ -142,6 +144,70 @@ class Terra_Data_Table implements ArrayAccess {
         ));
     }
 
+    function exportCode($var = 'table') {
+        $return = "\${$var} = new Terra_Data_Table('{$this['Name']}');\r\n";
+        if (!empty($this['Singular'])) {
+            $return .= "\${$var}->setSingular('{$this['Singular']}');\r\n";
+        }
+        if (!empty($this['Plural'])) {
+            $return .= "\${$var}->setPlural('{$this['Plural']}');\r\n";
+        }
+        if (!empty($this['Urls']['Manage'])) {
+            $return .= "\${$var}->setManageUrl('{$this['Urls']['Manage']}');\r\n";
+        }
+        if (!empty($this['Urls']['View'])) {
+            $return .= "\${$var}->setViewUrl('{$this['Urls']['View']}');\r\n";
+        }
+        if (!empty($this['Urls']['Edit'])) {
+            $return .= "\${$var}->setEditUrl('{$this['Urls']['Edit']}');\r\n";
+        }
+        if (!empty($this['Urls']['Create'])) {
+            $return .= "\${$var}->setCreateUrl('{$this['Urls']['Create']}');\r\n";
+        }
+        if (!empty($this['Urls']['Delete'])) {
+            $return .= "\${$var}->setDeleteUrl('{$this['Urls']['Delete']}');\r\n";
+        }
+        if (!empty($this['Urls']['Restore'])) {
+            $return .= "\${$var}->setRestoreUrl('{$this['Urls']['Restore']}');\r\n";
+        }
+        if ($this['HtmlTemplate'] != 'default') {
+            $return .= "\${$var}->setHtmlTemplate('{$this['HtmlTemplate']}');\r\n";
+        }
+        foreach ($this['Fields'] as $field) {
+            $return .= "\${$var}->addField('{$field['Identifier']}', '{$field['Name']}', '{$field['HumanName']}', ".(($field['Disabled']) ? 'true' : 'false').", '{$field['PrimaryKey']}');\r\n";
+            foreach ($field['ValidationRules'] as $rule => $arg) {
+                if ($rule == 'ExistsIn') {
+                    $return .= "\${$var}->existsIn('{$field['Identifier']}', '{$arg['Table']}', '{$arg['Field']}', '{$arg['Alias']}', '{$arg['ValueField']}');\r\n";
+                } else {
+                    if (is_string($arg) or is_int($arg)) {
+                        $return .= "\${$var}->addValidationRule('{$field['Identifier']}', '$rule', '$arg');\r\n";
+                    } elseif (is_array($arg)) {
+                        $return .= "\${$var}->addValidationRule('{$field['Identifier']}', '$rule', " . self::printArrayAsPhpCode($arg, true) . ");\r\n";
+                    }
+                }
+            }
+            if (isset($field['CanHave'])) {
+                if (isset($field['Alias'])) {
+                    $Alias = "'{$field['Alias']}'";
+                } else {
+                    $Alias = null;
+                }
+                if (isset($field['ValueField'])) {
+                    $ValueField = "'{$field['ValueField']}'";
+                } else {
+                    $ValueField = null;
+                }
+                $return .= "\${$var}->addRelationship('{$field['Identifier']}','{$field['CanHave']}','{$field['Table']}','{$field['Field']}','{$field['Rel_Table']}','{$field['ID']}','{$field['REL_ID']}', $Alias, $ValueField);\r\n";
+            }
+        }
+        
+        return $return;
+    }
+
+    function exportArray() {
+        return (array) $this;
+    }
+
     function addValidationRule($FieldIdentifier, $ValidationRule, $Argument) {
         $this->Container['Fields'][$FieldIdentifier]['ValidationRules'][$ValidationRule] = $Argument;
     }
@@ -163,6 +229,107 @@ class Terra_Data_Table implements ArrayAccess {
             $this->Container[] = $value;
         } else {
             $this->Container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Generate a table configuration array from a database table.
+     * 
+     * Grabs all the fields in a database table and transforms them into a table configuration array.
+     * This is to simplify the process of getting started with Terra Data.
+     * If $TerraDataTable is provided, discoverTable will fill it with data, rather than create a new one.
+     * 
+     * @param string $Table
+     * @param resource $Connection
+     * @param Terra_Data_Table $TerraDataTable
+     * @return Terra_Data_Table
+     */
+    public static function discoverTable($Table, $Connection = null, $TerraDataTable = null) {
+        if (!is_resource($Connection)) {
+            $Connection = Terra_Data_Connection::getConnection();
+            if (!is_resource($Connection)) {
+                throw new Terra_DataException("No working database connection was found.");
+            }
+        }
+
+        $Result = mysql_query("SHOW COLUMNS FROM $Table", $Connection);
+
+        if ($TerraDataTable instanceof Terra_Data_Table) {
+            $Return = &$TerraDataTable;
+        } else {
+            $Return = new Terra_Data_Table($Table);
+        }
+
+        while ($Row = mysql_fetch_assoc($Result)) {
+            $Identifier = $Row['Field'];
+            $Name = $Row['Field'];
+            $HumanName = ucwords(strtolower(str_ireplace('_', ' ', $Row['Field'])));
+            $PrimaryKey = ($Row['Key'] == 'PRI') ? true : false;
+            $Return->addField($Identifier, $Name, $HumanName, false, $PrimaryKey);
+
+            $Type = explode('(', $Row['Type']);
+            if (count($Type) > 1) {
+                # There were brackets. $Type[0] is the type, varchar or int. (int) $Type[1] is the MaxChars.
+                $Return->addValidationRule($Identifier, 'MaxChars', (int) $Type[1]);
+            }
+        }
+        return $Return;
+    }
+
+    /**
+     * Print an array (recursive) as PHP code (can be pasted into a php file and it will work).
+     * @param array $array
+     * @param boolean $return (whether to return or print the output)
+     * @return string|boolean (string if $return is true, true otherwise)
+     */
+    public static function printArrayAsPhpCode($array, $return = false) {
+        if (count($array) == 0) {
+            if (!$return) {
+                print "array()";
+                return true;
+            } else {
+                return "array()";
+            }
+        }
+        $string = "array(";
+        if (array_values($array) === $array) {
+            $no_keys = true;
+            foreach ($array as $value) {
+                if (is_int($value)) {
+                    $string .= "$value, ";
+                } elseif (is_array($value)) {
+                    $string .= printArrayInPHPFormat($value, true) . ",\n";
+                } elseif (is_string($value)) {
+                    $string .= "$value', ";
+                } else {
+                    trigger_error("Unsupported type of \$value, in index $key.");
+                }
+            }
+        } else {
+            $string .="\n";
+            foreach ($array as $key => $value) {
+                $no_keys = false;
+                if (is_int($value)) {
+                    $string .= "\"$key\" => $value,\n";
+                } elseif (is_array($value)) {
+                    $string .= "\"$key\" => " . printArrayInPHPFormat($value, true) . ",\n";
+                } elseif (is_string($value)) {
+                    $string .= "\"$key\" => '$value',\n";
+                } else {
+                    trigger_error("Unsupported type of \$value, in index $key.");
+                }
+            }
+        }
+        $string = substr($string, 0, strlen($string) - 2); # Remove last comma.
+        if (!$no_keys) {
+            $string .= "\n";
+        }
+        $string .= ")";
+        if (!$return) {
+            print $string;
+            return true;
+        } else {
+            return $string;
         }
     }
 
