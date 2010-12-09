85e39aaea33d3a279ddb37e9bf5375987254b8d9
bruno.terraduo@gmail.com <bruno.terraduo@gmail.com@c9d6c2dd-1101-386e-3e53-6a280b27bd08>
2010-12-09 10:39:31 +0000

    * Updated the Manage template; it's now a bit more full-featured.

--- trunk/lib/Terra_AppData/data/html_templates/default/manage.php
+++ trunk/lib/Terra_AppData/data/html_templates/default/manage.php
@@ -1,14 +1,25 @@
-<table>
-    <tr>
-        <?php foreach ($fields as $field) { ?>
-            <th><?php echo $field['HumanName']; ?></th>
-        <?php } ?>
-    </tr>
-    <?php foreach ($rows as $row) { ?>
+<table id="manage-<?php echo strtolower($records); ?>" class="manage terra-data">
+    <caption><?php echo ucwords($records); ?></caption>
+    <thead>
         <tr>
             <?php foreach ($fields as $field) { ?>
-                <td><?php echo $row[$field['Name']]; ?></td>
+                <th scope="col"><?php echo $field['HumanName']; ?></th>
             <?php } ?>
         </tr>
-    <?php } ?>
+    </thead>
+    <tfoot>
+        <td colspan="<?php echo count($fields); ?>">
+            <span class="page-footer">Page <?php echo $page ?> of <?php echo $total_pages; ?></span>
+            <span class="rows-footer">Showing <?php echo $rows_per_page; ?> <?php echo $records; ?></span>
+        </td>
+    </tfoot>
+    <tbody>
+        <?php foreach ($rows as $row) { ?>
+            <tr>
+                <?php foreach ($fields as $field) { ?>
+                    <td><?php echo $row[$field['Name']]; ?></td>
+                <?php } ?>
+            </tr>
+        <?php } ?>
+    </tbody>
 </table>
