401963f29edb224aa01a35480e9a75059ee5b5d6
bruno.terraduo@gmail.com <bruno.terraduo@gmail.com@c9d6c2dd-1101-386e-3e53-6a280b27bd08>
2010-12-09 02:54:18 +0000

    * Updated a few comments. Part of an ongoing effort to solve issue 14.

--- lib/Terra/Data/Connection.php
+++ lib/Terra/Data/Connection.php
@@ -1,13 +1,40 @@
 <?php
 
+/**
+ * Terra Data Connection
+ *
+ * Provides a container for a MySQL Connection, available anywhere.
+ *
+ * <code>
+ * # Setting a connection
+ * $connection = mysql_connect($host, $user, $pass);
+ * mysql_select_db($connection);
+ * Terra_Data_Connection::setConnection($connection);
+ *
+ * #And whenever you need to access that connection, just use:
+ * Terra_Data_Connection::getConnection();
+ * </code>
+ *
+ * @author Bruno De Barros <bruno@terraduo.com>
+ * @version 2
+ * @package Terra
+ * @subpackage Data
+ * @copyright Copyright (c) 2008-2011 Bruno De Barros.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 class Terra_Data_Connection {
 
-    private static $connection;
+    protected static $connection;
 
-    private function __construct() {
+    protected function __construct() {
         # Forbid instanciating.
     }
 
+    protected function __clone() {
+        # Forbid cloning.
+    }
+    
     public static function getConnection() {
         return self::$connection;
     }
@@ -15,4 +42,5 @@ class Terra_Data_Connection {
     public static function setConnection($connection) {
         self::$connection = $connection;
     }
+
 }
--- lib/Terra/DataException.php
+++ lib/Terra/DataException.php
@@ -5,10 +5,10 @@
  * Thrown whenever something wrong happens in Terra Data.
  *
  * @author Bruno De Barros <bruno@terraduo.com>
- * @version 1.0
+ * @version 2
  * @package Terra
  * @subpackage Data
- * @copyright Copyright (c) 2008-2010 Bruno De Barros.
+ * @copyright Copyright (c) 2008-2011 Bruno De Barros.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
