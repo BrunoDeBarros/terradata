ec7769599f8312b0c2cc628a47841adbce633f64
bruno.terraduo@gmail.com <bruno.terraduo@gmail.com@c9d6c2dd-1101-386e-3e53-6a280b27bd08>
2010-12-09 02:54:18 +0000

    * Updated a few comments. Part of an ongoing effort to solve issue 14.

--- trunk/lib/Terra/Autoload.php
+++ trunk/lib/Terra/Autoload.php
@@ -7,11 +7,21 @@
  * Does not interfere with other registered autoloaders.
  *
  * @author Bruno De Barros <bruno@terraduo.com>
- * @version 0.2
+ * @version 2
  * @package Terra
- * @copyright Copyright (c) 2008-2010 Bruno De Barros.
+ * @throws Terra_Exception if no valid Terra Application Data folder can be found.
+ * @copyright Copyright (c) 2008-2011 Bruno De Barros.
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
+ */
+
+/**
+ * Load the class with the name $class.
+ * 
+ * This function is to be called by PHP when autoloading a class, but can be called manually.
+ * 
+ * @param string $class
+ * @return false if the class with the name $class does not exist.
  */
 function Terra_Autoload($class) {
     # Don't interfere with other autoloaders.
@@ -21,7 +31,6 @@ function Terra_Autoload($class) {
     $path = dirname(__FILE__).'/'.str_replace('_', '/', substr($class, 6, strlen($class) - 6)).'.php';
 
     if (!file_exists($path)) {
-        die($path);
         return false;
     }
 
@@ -32,6 +41,9 @@ spl_autoload_register('Terra_Autoload');
 
 if (!defined('TERRA_APPDATA_PATH')) {
     if (file_exists(dirname(__FILE__).'../')) {
+        /**
+         * The path to the Terra Application Data folder.
+         */
         define('TERRA_APPDATA_PATH', dirname(__FILE__).'/../Terra_AppData/');
     } else {
         throw new Terra_Exception("A valid Terra_AppData folder was not found!");
