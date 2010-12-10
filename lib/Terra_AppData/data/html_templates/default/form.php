0684e584c837cdd8734f4c1234c6f2ab03e775c6
bruno.terraduo@gmail.com <bruno.terraduo@gmail.com@c9d6c2dd-1101-386e-3e53-6a280b27bd08>
2010-12-10 19:16:09 +0000

    Terra Data now saves all the logged queries (if logAllQueries is set to true), handles validation errors on the CreateController and can load ManageController from pre-set Page and RowsPerPage settings.

--- lib/Terra_AppData/data/html_templates/default/form.php
+++ lib/Terra_AppData/data/html_templates/default/form.php
@@ -3,10 +3,13 @@
         <legend>Legend Name</legend>
         <dl>
             <?php foreach ($fields as $field) { ?>
-                <dt><label for="<?php echo $field['Identifier'] ?>"><?php echo $field['HumanName'] ?></label></dt>
+                <dt>
+                <label for="field-<?php echo $field['Identifier'] ?>"><?php echo $field['HumanName'] ?></label>
+                <?php if ($field['Error']) { ?><span class="error"><?php echo $field['Error']; ?></span><?php } ?>
+                </dt>
                 <dd>
-                    <?php if (isset($field['ValidationRules']['Text']) and $field['ValidationRules']['MaxChars']) { ?>
-                        <input type="text" id="<?php echo $field['Identifier'] ?>" maxlength="<?php echo $field['ValidationRules']['MaxChars']; ?>" name="<?php echo $field['Identifier'] ?>" value="<?php echo $row[$field['Identifier']]; ?>" />
+                    <?php if (isset($field['ValidationRules']['Text']) and isset($field['ValidationRules']['MaxChars'])) { ?>
+                        <input type="text" class="Text MaxChars" id="field-<?php echo $field['Identifier'] ?>" maxlength="<?php echo $field['ValidationRules']['MaxChars']; ?>" name="fields[<?php echo $field['Identifier'] ?>]" value="<?php echo $row[$field['Identifier']]; ?>" />
                     <?php } ?>
                 </dd>
             <?php } ?>
